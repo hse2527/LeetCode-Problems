4edd773bae80093f394f5cb8c4ae7e3c259f9630
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2023-10-08 02:53:25 -0500

    Time: 10 ms (27.37%), Space: 43.5 MB (96.25%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        for(int i : nums) {
-            if(!set.add(i)) set.remove(i);
-        }
+        for(int i : nums) if(!set.add(i)) set.remove(i);
         return (Integer)(set.toArray()[0]);
     }
 }

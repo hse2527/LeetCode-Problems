fa0e05652321f408eadce61c6ede66d3ae5cb0a8
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2023-10-10 14:11:12 -0500

    Time: 125 ms (33.95%), Space: 55.1 MB (5.52%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,6 +5,7 @@ class Solution {
         int l, r;
         HashSet<String> set = new HashSet<>();
         for(int i = 0; i < nums.length - 2; i++) {
+            if(i != 0 && nums[i] == nums[i-1]) continue;
             int remaining = 0 - nums[i];
             l = i + 1;
             r = nums.length - 1;
@@ -23,6 +24,7 @@ class Solution {
         }
         return ans;
     }
+
     private String hash(Integer[] triplet) {
         return "" + triplet[0] + triplet[1] + triplet[2];
     }

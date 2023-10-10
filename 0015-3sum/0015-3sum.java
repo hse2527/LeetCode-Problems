d7777159fdf7842ad0c2c22f6800da4f9eaa51fa
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2023-10-10 14:09:29 -0500

    Time: 830 ms (6.36%), Space: 54.2 MB (5.52%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -24,6 +24,6 @@ class Solution {
         return ans;
     }
     private String hash(Integer[] triplet) {
-        return "" + triplet[0] + "" + triplet[1] + "" + triplet[2] + "";
+        return "" + triplet[0] + triplet[1] + triplet[2];
     }
 }

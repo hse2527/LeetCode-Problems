390eec2db103a978b877030c71efe0ed233ac821
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2023-10-10 14:00:40 -0500

    Time: 752 ms (6.96%), Space: 54.4 MB (5.52%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int l, r;
+        HashSet<String> set = new HashSet<>();
+        for(int i = 1; i < nums.length - 1; i++) {
+            int remaining = 0 - nums[i];
+            l = 0;
+            r = nums.length - 1;
+
+            while(l < r && l != i && r != i) {
+                Integer[] triplet = {nums[l], nums[i], nums[r]};
+                if(nums[l] + nums[r] > remaining) r--;
+                else if(nums[l] + nums[r] < remaining) l++;
+                else {
+                    if(set.add(hash(triplet))) {
+                        ans.add(Arrays.asList(triplet));
+                    }
+                    l++;
+                } 
+            }
+        }
+        return ans;
+    }
+    private String hash(Integer[] triplet) {
+        return "" + triplet[0] + "" + triplet[1] + "" + triplet[2] + "";
+    }
+}

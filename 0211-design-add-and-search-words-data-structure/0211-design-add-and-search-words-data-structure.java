5e7c8963f3588cfe1bd8e181c1acc1651f66a38e
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2023-10-16 21:37:23 -0500

    Time: 289 ms (22.62%), Space: 96.2 MB (68.3%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -1,45 +1,69 @@
+class TrieNode {
+    Map<Character, TrieNode> children;
+    boolean isWord;
+    
+    public TrieNode() {
+        children = new HashMap<>();
+        isWord = false;
+    }
+}
+
 class WordDictionary {
-    Map<Integer, List<String>> map = new HashMap<>();
-public void addWord(String word) {
-    int index = word.length();
-	if (!map.containsKey(index)) {
-	    List<String> list = new ArrayList<>();
-	    list.add(word);
-		map.put(index, list);
-	} else {
-		map.get(index).add(word);
-	}
-}
+    private TrieNode root;
 
+    public WordDictionary() {
+        root = new TrieNode();
+    }
 
-public boolean search(String word) {
-  int index = word.length();
-  if (!map.containsKey(index)) {
-      return false;
-  }
-  
-  List<String> list = map.get(index);
-  for(String s : list) { 
-      if(isSame(s, word)) { // when word has '.'
-          return true;
-      }
-  }
-  return false;
-}
+    public void addWord(String word) {
+        TrieNode node = root;
+        // Traverse the trie for each character in the word
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            // If the current node does not have a child with character c,
+            // create a new node and add it as a child of the current node
+            if (!node.children.containsKey(c)) {
+                node.children.put(c, new TrieNode());
+            }
+            // Move to the child node corresponding to character c
+            node = node.children.get(c);
+        }
+        // Mark the last node as a word node
+        node.isWord = true;
+    }
 
-public boolean isSame(String s, String word) { // word has '.'
-    for (int i = 0; i < s.length(); i++) {
-        if (word.charAt(i) != '.' && s.charAt(i) != word.charAt(i)) {
+    public boolean search(String word) {
+        return searchHelper(root, word, 0);
+    }
+
+    private boolean searchHelper(TrieNode node, String word, int index) {
+        // If we have reached the end of the word,
+        // check if the current node is a word node
+        if (index == word.length()) {
+            return node.isWord;
+        }
+        char c = word.charAt(index);
+        if (c == '.') {
+            // If the current character is a dot, we need to check all children of the current node
+            // recursively by skipping over the dot character and moving to the next character of the word
+            for (TrieNode child : node.children.values()) {
+                if (searchHelper(child, word, index + 1)) {
+                    return true;
+                }
+            }
+            // If no child node matches the remaining characters of the word,
+            // return false
             return false;
+        } else {
+            // If the current character is not a dot, move to the child node
+            // corresponding to that character and continue recursively
+            TrieNode child = node.children.get(c);
+            if (child == null) {
+                // If there is no child node corresponding to the current character,
+                // return false
+                return false;
+            }
+            return searchHelper(child, word, index + 1);
         }
     }
-    return true;
 }
-}
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary obj = new WordDictionary();
- * obj.addWord(word);
- * boolean param_2 = obj.search(word);
- */

c935eaa32c6ac41b99b477279806e2fad87d83dc
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2023-10-19 20:05:20 -0500

    Time: 28 ms (23.01%), Space: 42.4 MB (7.25%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,54 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    boolean[][] visited = new boolean[101][101];
+    Node[] visitedNodes = new Node[101];
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+        Node newNode = new Node(node.val);
+        visitedNodes[node.val] = newNode;
+        dfs(node, newNode, 1);
+
+        for(int i = 1; i <= 4; i++) {
+            for(int j = 1; j <= 4; j++) {
+                System.out.print(visited[i][j]);
+            }
+            System.out.println();
+        }
+        return newNode;
+    }
+
+    private void dfs(Node orig, Node newNode, int prev) {
+        visited[prev][orig.val] = true;
+        for(Node next : orig.neighbors) {
+            if(visited[orig.val][next.val]) continue;
+            Node newNext;
+            if(visitedNodes[next.val] == null) {
+                newNext = new Node(next.val);
+                visitedNodes[next.val] = newNext;
+            } else {
+                newNext = visitedNodes[next.val];
+            }
+            newNode.neighbors.add(newNext);
+            dfs(next, newNext, orig.val);
+        }
+    }
+}

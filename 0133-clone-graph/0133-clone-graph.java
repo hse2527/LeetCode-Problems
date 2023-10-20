d21d9ff8c5bdc8e134b610ec41e859922e7d4d57
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2023-10-19 20:13:44 -0500

    Time: 25 ms (95.75%), Space: 42.3 MB (7.25%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,23 +1,3 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
-
 class Solution {
     boolean[][] visited = new boolean[101][101];
     Node[] visitedNodes = new Node[101];
@@ -25,7 +5,7 @@ class Solution {
         if(node == null) return null;
         Node newNode = new Node(node.val);
         visitedNodes[node.val] = newNode;
-        dfs(node, newNode, 1);
+        dfs(node, newNode, node.val);
         return newNode;
     }
 
@@ -33,13 +13,15 @@ class Solution {
         visited[prev][orig.val] = true;
         for(Node next : orig.neighbors) {
             if(visited[orig.val][next.val]) continue;
-            Node newNext;
-            if(visitedNodes[next.val] == null) {
-                newNext = new Node(next.val);
-                visitedNodes[next.val] = newNext;
-            } else {
-                newNext = visitedNodes[next.val];
-            }
+            Node newNext = (visitedNodes[next.val] == null) ? new Node(next.val) : visitedNodes[next.val];
+            visitedNodes[next.val] = newNext;
+            
+            // if(visitedNodes[next.val] == null) {
+            //     newNext = new Node(next.val);
+            //     visitedNodes[next.val] = newNext;
+            // } else {
+            //     newNext = visitedNodes[next.val];
+            // }
             newNode.neighbors.add(newNext);
             dfs(next, newNext, orig.val);
         }

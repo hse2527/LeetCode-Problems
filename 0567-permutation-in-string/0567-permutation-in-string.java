b80ecbadef3202f1d9d5fd067ec9cfb429186030
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2023-10-29 19:47:54 -0500

    Time: 7 ms (72.42%), Space: 41.4 MB (88.4%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,21 +1,38 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        char[] orig = s1.toCharArray();
-        Arrays.sort(orig);
-        s1 = new String(orig);
-        int a = 0;
-        int b = s1.length();
-        while(b <= s2.length()) {
-            if(checkParts(s1, s2.substring(a, b).toCharArray())) return true;
-            a++;
-            b++;
+        int[] count = new int[26];
+        for(int i = 0; i < 26; i++) count[i] = -1;
+        for(char c : s1.toCharArray()) {
+            if(count[c - 'a'] == -1) count[c - 'a'] = 0;
+            count[c-'a']++;
         }
-        return false;
-    }
-
-    public boolean checkParts(String orig, char[] comp) {
-        Arrays.sort(comp);
-        if(new String(orig).equals(new String(comp))) return true;
-        return false;
+        int a = 0;
+        int b = 0;
+        while(b < s2.length()) {
+            if(b - a == s1.length()) return true;
+            if(count[s2.charAt(b) - 'a'] == 0) {
+                while(count[s2.charAt(b) - 'a'] == 0) {
+                    count[s2.charAt(a) - 'a']++;
+                    a++;
+                }
+                count[s2.charAt(b) - 'a']--;
+                b++;
+            } else if (count[s2.charAt(b) - 'a'] == -1) {
+                if(count[s2.charAt(a) - 'a'] != -1) {
+                    while(a != b) {
+                        count[s2.charAt(a) - 'a']++;
+                        a++;
+                    }
+                    a++;
+                } else {
+                    a++;
+                }
+                b++;
+            } else {
+                count[s2.charAt(b) - 'a']--;
+                b++;
+            }
+        }
+        return (b - a == s1.length()) ? true : false;
     }
 }

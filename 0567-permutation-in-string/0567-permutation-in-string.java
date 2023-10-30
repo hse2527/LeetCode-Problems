72a5e0b88dbb989b5bbb37231bdfd0f26fe21e7c
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2023-10-29 19:10:17 -0500

    Time: 808 ms (6.27%), Space: 44.3 MB (6.45%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,17 +2,18 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         char[] orig = s1.toCharArray();
         Arrays.sort(orig);
+        s1 = new String(orig);
         int a = 0;
         int b = s1.length();
         while(b <= s2.length()) {
-            if(checkParts(orig, s2.substring(a, b).toCharArray())) return true;
+            if(checkParts(s1, s2.substring(a, b).toCharArray())) return true;
             a++;
             b++;
         }
         return false;
     }
 
-    public boolean checkParts(char[] orig, char[] comp) {
+    public boolean checkParts(String orig, char[] comp) {
         Arrays.sort(comp);
         if(new String(orig).equals(new String(comp))) return true;
         return false;

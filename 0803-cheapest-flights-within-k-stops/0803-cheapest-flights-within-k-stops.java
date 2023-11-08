9b3da235375e21c853d3578b85385b5d14b1ba1a
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2023-11-08 01:34:28 -0600

    Time: 20 ms (8.56%), Space: 45.3 MB (5.22%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -1,29 +1,43 @@
 class Solution {
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] distance = new int[n];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        distance[src] = 0;
-        for (int i = 0; i <= k; i++) {
-            if (isRoutePossible(distance, flights)) {
-                break;
-            }
+    class Route {
+        int dst;
+        int price;
+        int rem;
+        Route(int dst, int price, int rem) {
+            this.dst = dst;
+            this.price = price;
+            this.rem = rem;
         }
-        return distance[dst] == Integer.MAX_VALUE ? -1 : distance[dst];
     }
-    private boolean isRoutePossible(int[] dist, int[][] flights) {
-        int[] copy = Arrays.copyOf(dist, dist.length);
-        boolean result = true;
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        HashMap<Integer, ArrayList<int[]>> map = new HashMap<>();
+        for(int[] flight : flights) {
+            ArrayList list = map.getOrDefault(flight[0], new ArrayList<int[]>());
+            list.add(new int[]{flight[1],flight[2]});
+            map.put(flight[0], list);
+        }
+        if(map.get(src) == null) return -1;
 
-        for (int[] flight : flights) {
-            int src = flight[0];
-            int dst = flight[1];
-            int cost = flight[2];
-
-            if (copy[src] < Integer.MAX_VALUE && dist[dst] > dist[src] + cost) {
-                dist[dst] = cost + copy[src];
-                result = false;
+        PriorityQueue<Route> pq = new PriorityQueue<>((a, b) -> a.price - b.price);
+        int[] weight = new int[n];
+        int[] rems = new int[n];
+        Arrays.fill(weight, Integer.MAX_VALUE);
+        weight[src] = 0;
+        for(int[] route : map.get(src)) pq.add(new Route(route[0],route[1],k));
+        while(!pq.isEmpty()) {
+            Route cur = pq.poll();
+            if(cur.dst == 1)System.out.println(cur.dst + " " + cur.rem + " " + cur.price);
+            if(cur.dst == dst) return cur.price;
+            if(cur.rem == 0 || map.get(cur.dst) == null) continue;
+            weight[cur.dst] = cur.price;
+            rems[cur.dst] = cur.rem;
+            
+            for(int[] flight : map.get(cur.dst)) {
+                if((cur.rem == 1 && flight[0] != dst) || 
+                    (weight[flight[0]] < flight[1] + cur.price && rems[flight[0]] > cur.rem - 1)) continue;
+                pq.add(new Route(flight[0], cur.price + flight[1], cur.rem - 1));
             }
         }
-        return result;
+        return -1;
     }
 }

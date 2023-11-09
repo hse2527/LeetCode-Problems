06282dc144ba3bbe35d8d3e834b315b6a2c1a42c
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2023-11-10 06:56:33 +0900

    Time: 2 ms (23.31%), Space: 43.4 MB (48.47%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    HashSet<String> set = new HashSet();
+    List<String> ans = new ArrayList();
+    int lim;
+
+    public List<String> generateParenthesis(int n) {
+        int opens = n;
+        int closes = n;
+        lim = 2*n;
+        recursivef(opens, closes, "");
+        return ans;
+    }
+
+    public void recursivef(int opens, int closes, String cur) {
+        if(cur.length() == lim) {
+            if(!set.contains(cur)) {
+                set.add(cur);
+                ans.add(cur);
+            }
+            return;
+        }
+        if(cur.length() == 0) {
+            recursivef(opens - 1, closes, cur + "(");
+        } else {
+            if (opens > 0) recursivef(opens - 1, closes, cur + "(");
+            if ((closes > 1 || cur.length() == lim - 1) 
+                && opens < closes) {
+                recursivef(opens, closes - 1, cur + ")");
+            }
+        }
+        
+    }
+}
